95e4e5f757062d4d0cd382e78c2bbf50a56c5038
Kevin Ngo <k.ngo93@outlook.com>
2018-12-25 20:29:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,22 +21,3 @@ function destructivelyRemoveFirstKitten() {
   return kittens;
 }
 
-function appendKitten(name){
-  var new = kittens.push(name);
-  return new;
-}
-
-function prependKitten(name) {
-  var new = [name, ...kittens];
-  return new;
-}
-
-function removeLastKitten() {
-  var new = kittens.slice(0, kittens.length - 1);
-  return new;
-}
-
-function removeFirstKitten() {
-  var new = kittens.slice(1, kittens.length);
-  return new;
-}
