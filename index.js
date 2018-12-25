1c7fd4000a24a60ada47d492bcee9278a6d37160
Kevin Ngo <k.ngo93@outlook.com>
2018-12-25 20:33:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,9 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
   var newArray = [...kittens, name];
   return newArray;
+}
 
+function prependKitten(name) {
+  var newArray = [name, ...kittens];
+  return newArray;
 }
